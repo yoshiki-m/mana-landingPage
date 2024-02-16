d8e043f09f66839293cb21f947fc38cee8cd5469
yoshiki-m <m.yoshiki0124@gmail.com>
2024-02-16 11:12:47 +0900

    サイト移行対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import CallToActionWithVideo from "./components/hero_header"
-import AboutBim from "./components/about_bim"
-import AboutSystemDevelopment from "./components/about_system_development"
-import Contact from "./components/contact"
-import CEOIntroduction from "./components/ceo_introduction"
-import Footer from "./components/footer"
+import * as React from "react";
+import { ChakraProvider, Box, Grid, theme, Text, Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import CallToActionWithVideo from "./components/hero_header";
+import AboutBim from "./components/about_bim";
+import AboutSystemDevelopment from "./components/about_system_development";
+import Contact from "./components/contact";
+import CEOIntroduction from "./components/ceo_introduction";
+import Footer from "./components/footer";
 
 export const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
+        <Text
+          id={"top"}
+          as={"span"}
+          position={"relative"}
+          fontSize={{ base: "1xl", sm: "2xl", lg: "3xl" }}
+        >
+          株式会社manaのサイトは、以下に移動しました。
+          <br />
+        </Text>
+        <Link href="https://www.mana-it-arch.com/" isExternal>
+          新しいサイトはこちらです。 <ExternalLinkIcon mx="2px" />
+        </Link>
         <Grid p={3}>
-          <CallToActionWithVideo/>
+          <CallToActionWithVideo />
         </Grid>
       </Box>
-      <AboutBim/>
-      <AboutSystemDevelopment/>
+      <AboutBim />
+      <AboutSystemDevelopment />
       <Contact />
       <CEOIntroduction />
       <Footer />
     </ChakraProvider>
-  )
-}
+  );
+};
